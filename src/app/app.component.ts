8e2f18625e52b1e5cf57c84dc20d05575c8ae41f
Hezi Doryon <hdoryon@gmail.com>
2017-01-21 17:35:35 +0200

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import {Component} from '@angular/core';
+//import {NgRedux} from "ng2-redux";
+//import {rootReducer} from "./store/index";
+@Component({
+    selector: 'my-app',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+
+    /*constructor(private ngRedux: NgRedux<GymStatus>) {
+        this.ngRedux.configureStore(rootReducer, {});
+    }*/
+
+}
